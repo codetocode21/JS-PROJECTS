47a581da7ae3ee881ad36e27e1c94b3f20f21ca9
arLong-98 <dhairya999pateriya@gmail.com>
2022-11-17 13:55:11 +0530

    minor update

--- ColorFlipper/hex.js
+++ ColorFlipper/hex.js
@@ -6,13 +6,13 @@ const color = document.querySelector(".color");
 btn.addEventListener('click', function(){
 let hexColor = '#';
 for(let i = 0; i < 6; i++){
- hexColor += hex[getRandomNumber()]; 
+ hexColor += hex[getRandomNumberNew()]; 
 }
 
 color.textContent = hexColor;
 document.body.style.backgroundColor = hexColor;
 });
 
-function getRandomNumber(){
+function getRandomNumberNew(){
     return Math.floor(Math.random() * hex.length)
-} 
+}
